4bd0ef2aa031b3fabb85ec5ff250fece008857cb
tard <hadjik777@gmail.com>
2023-09-18 15:37:02 +0300

    now screen clears on exit

--- kilo.c
+++ kilo.c
@@ -18,6 +18,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -53,10 +56,7 @@ char editorReadKey() {
 
 /*** output ***/
 
-void editorRefreshScreen() {
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-	write(STDOUT_FILENO, "\x1b[H", 3);
-}
+
 
 /*** input ***/
 
@@ -65,6 +65,8 @@ void editorProcessKeypress() {
 
 	switch (c) {
 		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
 	}
@@ -76,7 +78,6 @@ int main() {
 	enableRawMode();
 
 	while (1) {
-		editorRefreshScreen();
 		editorProcessKeypress();
 	}
 
